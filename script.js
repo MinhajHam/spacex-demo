a0748d8322b8a636a07ce408bfd151d62c471fb8
Minhaj Hameed <minhajham@yahoo.com>
2023-02-24 21:42:12 +0530

    add doc elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById('menu-btn');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
